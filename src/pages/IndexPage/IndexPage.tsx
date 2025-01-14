ac0a3bb0cc3ab10fe7c6d54c2f718038073bb6e6
limkimheng <79346568+limkimheng@users.noreply.github.com>
2025-01-14 15:46:57 +0700

    update

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -54,52 +54,5 @@ export const IndexPage: FC = () => {
       <Button size="m" mode='white' stretched>SUBMIT  </Button>
     </Section>
     </List>
-    // <center >
-    //     <Image
-    //           src={icAppSvg}
-    //           alt="appicon"
-    //           width={250}
-    //           height={250}
-    //      />
-         
-    //      <span>Mobile Banking</span>
-    // </center>
-    // <Page back={false} >
-    //   <List  align-items="center" >
-    //     <Section
-    //       style={{ backgroundColor: '#ffffff' ,textAlign: 'center'}}
-    //       header="Create New Account"  
-    //       //footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
-    //     >
-    //     </Section>
-    //     <Section   align-items="center">   
-    //         <Cell   
-    //           before={<Image src={icAppSvg} style={{ backgroundColor: '#ffffff' }}/>}
-    //           subtitle="Please enter your phone number"//  style={{ backgroundColor: '#ffffff' }}
-            
-    //        >
-    //           Mobile Banking  
-    //         </Cell>
-
-    //     </Section>
-        
-    //     <Section
-    //     style={{ backgroundColor: '#ffffff' }}
-    //       header="Application Launch Data" 
-    //       footer="These pages help developer to learn more about current launch information"
-          
-    //     >
-    //       <Link to="/init-data">
-    //         <Cell subtitle="User data, chat information, technical data">Init Data</Cell>
-    //       </Link>
-    //       <Link to="/launch-params">
-    //         <Cell subtitle="Platform identifier, Mini Apps version, etc.">Launch Parameters</Cell>
-    //       </Link>
-    //       <Link to="/theme-params">
-    //         <Cell subtitle="Telegram application palette information">Theme Parameters</Cell>
-    //       </Link>
-    //     </Section>
-    //   </List>
-    // </Page>
   );
 };
