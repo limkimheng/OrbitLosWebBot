4d6b44dc534af96bd1f9f41a987cf42652562206
limkimheng <79346568+limkimheng@users.noreply.github.com>
2025-01-14 16:30:23 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,14 @@
-import ReactDOM from 'react-dom/client';
-import { StrictMode } from 'react';
-import { retrieveLaunchParams } from '@telegram-apps/sdk-react';
-
-import { Root } from '@/components/Root.tsx';
-import { EnvUnsupported } from '@/components/EnvUnsupported.tsx';
-import { init } from '@/init.ts';
-
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import './index.css';
 
-// Mock the environment in case, we are outside Telegram.
-import './mockEnv.ts';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+import { setBackgroundAsSecondary } from './helpers/setBackgroundAsSecondary';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+setBackgroundAsSecondary();
 
-try {
-  // Configure all application dependencies.
-  init(retrieveLaunchParams().startParam === 'debug' || import.meta.env.DEV);
+const root = createRoot(
+  document.getElementById('root') as HTMLElement
+);
 
-  root.render(
-    <StrictMode>
-      <Root/>
-    </StrictMode>,
-  );
-} catch (e) {
-  root.render(<EnvUnsupported/>);
-}
+root.render(<App />);
